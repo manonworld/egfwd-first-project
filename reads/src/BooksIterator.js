828e862844fdc497e3f6760f8d54c8a0d38c0870
mosta <info@manonworld.de>
2021-09-10 19:32:25 +0200

    feat(added list books) Added List of Books as recommended books preloaded before the search action Added List of books on shelves
    Closes #5 Closes #6

---
+++ reads/src/BooksIterator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function BooksIterator ( props ) {
+    return props.books && props.books.length > 0 ? props.books.map( ( book ) => (
+        <div className="book">
+            <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}></div>
+            <div className="book-shelf-changer">
+                <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none" selected>None</option>
+                </select>
+            </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            {book.authors ? book.authors.map(( author ) => (
+                <div className="book-authors">{author}</div>
+            )) : 'No Authors'}
+        </div>
+    )) : <div>No Results</div>;
+}
+
+export default BooksIterator;
